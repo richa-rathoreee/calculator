6d7e011e1fe03ba5ad96e08c62b7ac9f7269a55f
Richa Rathore <richa.rathore.walkingtree.tech>
2021-07-23 10:35:42 +0530

    basic structure of calc

---
+++ app/calc/calc.component.ts
@@ -0,0 +1,28 @@
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent implements OnInit {
+
+  
+// @ViewChild("container") con:ElementRef<any>
+  constructor() { 
+    // console.log(this.container)
+    let container= document.querySelector(".upper") as HTMLElement
+    console.log(container);
+
+  //    container!.addEventListener("click", (event) => {
+  //     console.log(event);
+  //     console.log(event.target);
+  //     // event.target.style.backgroundColor = "yellow"
+  
+  // })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
