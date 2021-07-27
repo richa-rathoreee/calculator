f1c6a67d92cfa07b880f55c3888df0953bbaa627
Richa Rathore <richa.rathore.walkingtree.tech>
2021-07-27 14:18:06 +0530

    uploaded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalcComponent } from './calc/calc.component';
-import { CalccComponent } from './calcc/calcc.component';
+
 import { StringComponent } from './string/string.component';
-// import { CalBodyComponent } from './cal-body/cal-body.component';
-import { ToggleComponent } from './toggle/toggle.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     CalcComponent,
-    CalccComponent,
     StringComponent,
-    // CalBodyComponent,
-    ToggleComponent,
+ 
+
     
     
   ],
